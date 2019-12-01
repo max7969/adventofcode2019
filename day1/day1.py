6d7cc510fbcdc5ffff49ace4a2394af7302da03a
Andre Maxime <maxime.andre@zenika.com>
2019-12-01 10:54:55 +0100

    day1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -8,16 +8,42 @@ def base_path():
     return os.path.join("day1")
 
 
-def solution(file_path):
+def get_lines(file_path):
     file = open(os.path.join(base_path(), file_path), "r")
+    return file.readlines()
+
+
+def fuel_per_module(weight):
+    return (math.trunc(weight / 3.0) - 2)
+
+
+def fuel_per_module2(weight):
+    fuel = fuel_per_module(weight)
+    additional_fuel = fuel_per_module(fuel)
+    while additional_fuel > 0:
+        fuel += additional_fuel
+        additional_fuel = fuel_per_module(additional_fuel)
+    return fuel
+
+
+def compute(lines, fuel_function):
     sum = 0
-    for line in file.readlines():
-        sum += math.floor(int(line.rstrip()) / 3.0) - 2
+    for line in lines:
+        sum += fuel_function(int(line.rstrip()))
     return sum
+
+
+def solution(file_path):
+    return compute(get_lines(file_path), fuel_per_module)
+
+
+def solution2(file_path):
+    return compute(get_lines(file_path), fuel_per_module2)
 
 
 def main():
     print(solution(os.path.join("inputs", "input")))
+    print(solution2(os.path.join("inputs", "input")))
 
 
 if __name__ == "__main__":
--- day1/tests/test_day1.py
+++ day1/tests/test_day1.py
@@ -6,3 +6,9 @@ def test_day1():
     result = day1.solution(os.path.join(
         "tests", "resources", "test_1"))
     assert result == 34241
+
+
+def test_day1_part2():
+    result = day1.solution2(os.path.join(
+        "tests", "resources", "test_1"))
+    assert result == 51316
