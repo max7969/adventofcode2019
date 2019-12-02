52b11ad308acae4f59f62f72d0e0d4123e104dd3
Andre Maxime <maxime.andre@zenika.com>
2019-12-02 20:51:00 +0100

    refacto

--- day2/day2.py
+++ day2/day2.py
@@ -3,20 +3,20 @@ import file_utils
 
 
 def compute(program):
-    instrs = [int(i) for i in program.split(",")]
-    index = 0
-    while instrs[index] != 99 or index > (len(instrs) - 1):
-        if instrs[index] == 1:
-            instrs[instrs[index + 3]] = instrs[instrs[index + 1]] + \
-                instrs[instrs[index + 2]]
-        elif instrs[index] == 2:
-            instrs[instrs[index + 3]] = instrs[instrs[index + 1]] * \
-                instrs[instrs[index + 2]]
-        index = index + 4
-    return ",".join([str(i) for i in instrs])
+    memory = [int(i) for i in program.split(",")]
+    addrOp = 0
+    while memory[addrOp] != 99 or addrOp > (len(memory) - 1):
+        param1, param2 = memory[memory[addrOp + 1]], memory[memory[addrOp + 2]]
+        addrResult = memory[addrOp + 3]
+        if memory[addrOp] == 1:
+            memory[addrResult] = param1 + param2
+        elif memory[addrOp] == 2:
+            memory[addrResult] = param1 * param2
+        addrOp = addrOp + 4
+    return ",".join([str(i) for i in memory])
 
 
-def fix_program(program, noun, verb):
+def fix_program(program, noun=12, verb=2):
     split = program.split(",")
     split[1] = str(noun)
     split[2] = str(verb)
@@ -26,8 +26,7 @@ def fix_program(program, noun, verb):
 def find_noun_and_verb(solution, program):
     for noun in range(100):
         for verb in range(100):
-            fixed_program = fix_program(program, noun, verb)
-            if solution == int(compute(fixed_program).split(",")[0]):
+            if solution == int(compute(fix_program(program, noun, verb)).split(",")[0]):
                 return str(100 * noun + verb)
     return "error"
 
@@ -37,7 +36,7 @@ def open_program(path_name, file_name):
 
 
 def solution(path_name, file_name):
-    program = fix_program(open_program(path_name, file_name), 12, 2)
+    program = fix_program(open_program(path_name, file_name))
     return compute(program).split(",")[0]
 
 
