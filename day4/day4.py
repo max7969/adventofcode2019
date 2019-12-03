73a499178b5f791f2f1095b71fbf148d9bb7e2e1
Andre Maxime <maxime.andre@zenika.com>
2019-12-03 20:45:44 +0100

    init day4

---
+++ day4/day4.py
@@ -0,0 +1,9 @@
+import os
+import file_utils
+
+
+def solution(path_name, file_name):
+    return 1
+
+
+print(solution("day4/inputs", "input"))
---
+++ day4/test_day4.py
@@ -0,0 +1,6 @@
+import os
+import day4
+
+
+def test1_day4():
+    assert 1 == 1
