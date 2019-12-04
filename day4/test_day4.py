aacd78864ed420b895d7781490be47e08d5daa0d
Andre Maxime <maxime.andre@zenika.com>
2019-12-04 19:01:39 +0100

    fix tests day4

--- day4/test_day4.py
+++ day4/test_day4.py
@@ -3,19 +3,24 @@ import day4
 
 
 def test1_day4():
-    assert len(day4.filter_passwords(["223450"])) == 0
+    assert len(day4.filter_passwords_softlty(["223450"])) == 0
+
 
 def test2_day4():
-    assert len(day4.filter_passwords(["111111"])) == 1
+    assert len(day4.filter_passwords_softlty(["111111"])) == 1
+
 
 def test3_day4():
-    assert len(day4.filter_passwords(["123789"])) == 0
+    assert len(day4.filter_passwords_softlty(["123789"])) == 0
+
 
 def test4_day4():
-    assert len(day4.filter_passwords_2(["112233"])) == 1
+    assert len(day4.filter_passwords_strongly(["112233"])) == 1
+
 
 def test5_day4():
-    assert len(day4.filter_passwords_2(["123444"])) == 0
+    assert len(day4.filter_passwords_strongly(["123444"])) == 0
+
 
 def test6_day4():
-    assert len(day4.filter_passwords_2(["111122"])) == 1
+    assert len(day4.filter_passwords_strongly(["111122"])) == 1
