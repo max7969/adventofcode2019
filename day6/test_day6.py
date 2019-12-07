98c61dbc6590d8466c1466e4a67ad09ae598400c
Andre Maxime <maxime.andre@zenika.com>
2019-12-07 02:16:57 +0100

    fix tests

--- day6/test_day6.py
+++ day6/test_day6.py
@@ -3,10 +3,10 @@ import day6
 
 
 def test1_day6():
-    result = day6.solution("day6/inputs", "test_1")
+    result = day6.solution("day6/inputs", "test_1", day6.count)
     assert result == 42
 
 
 def test2_day6():
-    result = day6.solution2("day6/inputs", "test_2")
+    result = day6.solution("day6/inputs", "test_2", day6.count_moves)
     assert result == 4
