124ac6b7cef25014cd30ac6b925de1a05535c2a1
maxime.andre <maxime.andre@ext.cdiscount.com>
2019-12-10 18:39:39 +0100

    day10 part2

--- day10/day10.py
+++ day10/day10.py
@@ -2,6 +2,7 @@ import os
 import file_utils
 import math
 from itertools import permutations
+import time
 
 
 def extract_asteroids(map):
@@ -25,29 +26,59 @@ def compute_angle(a, b):
     nv1, nv2 = normalize(v1), normalize(v2)
     cos = (v1[0] * v2[0] + v1[1] * v2[1]) / (nv1 * nv2)
     sin = (v1[0] * v2[1] - v1[1] * v2[0])
-    return round(math.copysign(1, sin) * math.acos(cos), 4)
+    angle = round(math.copysign(1, sin) * math.acos(cos), 4)
+    if angle >= 0:
+        return angle
+    else:
+        return round(math.pi*2 + angle, 4)
 
-def find_angles(asteroids):
-    max = 0
-    point = asteroids[0]
+def dist(center, asteroid):
+    return abs(center[0] - asteroid[0]) + abs(center[1] - asteroid[1])
+
+def find_angles(center, asteroids):
+    angles = []
     for asteroid in asteroids:
-        angles = set()
-        for asteroid_comparison in asteroids:
-            if asteroid != asteroid_comparison:
-                angles.add(compute_angle(asteroid, asteroid_comparison))
+        if center != asteroid:
+            angles.append([compute_angle(center, asteroid), dist(center, asteroid), asteroid])
+    return angles
+
+def find_best_asteroid(asteroids):
+    max, center, possible_angles = 0, asteroids[0], []
+    for asteroid in asteroids:
+        angles = set([element[0] for element in find_angles(asteroid, asteroids)])
         if len(angles) > max:
             max = len(angles)
-            point = asteroid
-    return max, point
+            center = asteroid
+            possible_angles = angles
+    return max, center, possible_angles
 
+def find_200th_destroyed_asteroid(asteroids):
+    _, center, possible_angles = find_best_asteroid(asteroids)
+    angles = find_angles(center, asteroids)
+    possible_angles = sorted(angles, key=lambda x: x)
+    angles = sorted(angles, key=lambda x: (x[0], x[1], x[0]))
+    value = angles[0][0]
+    count = 0
+    for angle in angles:
+        if angle[0] == value:
+            angle.append(count)
+            count += 1
+        else:
+            value = angle[0]
+            count = 0
+            angle.append(count)
+    angles = sorted(angles, key=lambda x: (x[3], x[0]))
+    return angles[199][2][0] * 100 + angles[199][2][1]
 
-def solution(path_name, file_name):
+def solution(path_name, file_name, function):
     map = file_utils.get_lines(path_name, file_name)
     asteroids = extract_asteroids(map)
-    max, point = find_angles(asteroids)
-    return max
+    return function(asteroids)
 
 
 if __name__ == "__main__":
-    print(solution("day10/inputs", "input"))
+    start_time = time.time()
+    #print(solution("day10/inputs", "input", find_best_asteroid))
+    print(solution("day10/inputs", "input", find_200th_destroyed_asteroid))
+    print("--- %s seconds ---" % (time.time() - start_time))
 
--- day10/test_day10.py
+++ day10/test_day10.py
@@ -3,27 +3,27 @@ import day10
 
 
 def test1_day10():
-    result, point = day10.solution("day10/inputs", "test_1")
+    result, point = day10.solution("day10/inputs", "test_1", day10.find_best_asteroid)
     assert result == 8
     assert point == [3,4]
 
 def test2_day10():
-    result, point = day10.solution("day10/inputs", "test_2")
+    result, point = day10.solution("day10/inputs", "test_2", day10.find_best_asteroid)
     assert result == 33
     assert point == [5,8]
 
 def test3_day10():
-    result, point = day10.solution("day10/inputs", "test_3")
+    result, point = day10.solution("day10/inputs", "test_3", day10.find_best_asteroid)
     assert result == 35
     assert point == [1,2]
 
 def test4_day10():
-    result, point = day10.solution("day10/inputs", "test_4")
+    result, point = day10.solution("day10/inputs", "test_4", day10.find_best_asteroid)
     assert result == 41
     assert point == [6,3]
 
 def test5_day10():
-    result, point = day10.solution("day10/inputs", "test_5")
+    result, point = day10.solution("day10/inputs", "test_5", day10.find_best_asteroid)
     assert result == 210
     assert point == [11,13]
 
